df74ad3ef2e51a0f1aae2b3f187b4a5a4e9af174
fedallxn <faithe.dall@gmail.com>
2026-03-20 17:43:38 -0500

    Created a images folder in assets to accommedate the 6 images I downloaded of the plants that are in the mockData file. Speaking of, I also created a mockPlantData file that has 6 differents plants. This will be used as the base content when a user opens the collection page.

---
+++ unit-1-final-project/src/components/mockPlantData.js
@@ -0,0 +1,83 @@
+import pothos from './assets/plantImages/pothos.png'
+import monstera from './assets/plantImages/monstera.png'
+import spiderPlant from './assets/plantImages/spiderPlant.png'
+import snakePlant from './assets/plantImages/snakePlant.png'
+import fiddldLeafFig from './assets/plantImages/fiddleLeafFig.png'
+import peaceLily from './assets/plantImages/peaceLily.png'
+
+const mockPlantData = [
+    {
+        id: 1,
+        image: pothos,
+        imageAlt: 'A photo of a small hanging pothos plant with green heart-shaped leaves',
+        health: 100,
+        lastWartered: '03-10-2026',
+        name: 'Pothos',
+        species: 'Epipremnum aureum',
+        sunlightNeeds: 'Low to Medium',
+        isToxic: true,
+        wateringFrequency: 7
+    },
+    {
+        id: 2,
+        image: monstera,
+        imageAlt: 'A photot of a large monstera plant with large split leaves',
+        health: 100,
+        lastWartered: '03-08-2026',
+        name: 'Monstera',
+        species: 'Monstera deliciosa',
+        sunlightNeeds: 'Medium to Bright Indirect',
+        isToxic: true,
+        wateringFrequency: 10
+    },
+    {
+        id: 3,
+        image: spiderPlant,
+        imageAlt: 'A photo of a large spider plant with arching green and white striated leaves in a glossy, blue and white patterned pot',
+        health: 100,
+        lastWartered: '03-14-2026',
+        name: 'Spider Plant',
+        species: 'Chlorophytum comosum',
+        sunlightNeeds: 'Bright Indirect',
+        isToxic: false,
+        wateringFrequency: 7
+    },
+    {
+        id: 4,
+        image: snakePlant,
+        imageAlt: 'A photo of a snake plant with tall swoard shaped leaves that are striped with with different colors of green',
+        health: 100,
+        lastWartered: '03-05-2026',
+        name: 'Snake Plant',
+        species: 'Sansevieria trifasciata',
+        sunlightNeeds: 'Low to Bright Indirect',
+        isToxic: true,
+        wateringFrequency: 14
+    },
+    {
+        id: 5,
+        image: fiddldLeafFig,
+        imageAlt: 'A photo of a fiddle leaf fig plant with large violin shaped glassy leaves',
+        health: 100,
+        lastWartered: '03-15-2026',
+        name: 'Fiddle leaf Fig',
+        species: 'Ficus lyrata',
+        sunlightNeeds: 'Bright Indirect',
+        isToxic: true,
+        wateringFrequency: 10
+    },
+    {
+        id: 6,
+        image: peaceLily,
+        imageAlt: 'A photo of a peace lily with dark green leaves and white blossoms with a spikey stamen',
+        health: 100,
+        lastWartered: '03-12-2026',
+        name: 'Peace Lily',
+        species: 'Spathiphyllum wallisii',
+        sunlightNeeds: 'Low to Medium Indirect',
+        isToxic: true,
+        wateringFrequency: 7
+    }
+]
+
+export default mockPlantData;
