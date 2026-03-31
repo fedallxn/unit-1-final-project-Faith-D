023627f0d2ff4c543e0037f110bfedc84e9347e9
fedallxn <faithe.dall@gmail.com>
2026-03-31 11:22:23 -0500

    Created the plant form so that when the user wants to add a plant, a form will pop up and allow the user to enter the plants details. Since there's no database, I added a placeholder image for when a user adds a new plant so the plant cards structure stays consistent. The plant will be added to the end of the list.

--- unit-1-final-project/src/components/AddPlantForm.jsx
+++ unit-1-final-project/src/components/AddPlantForm.jsx
@@ -1,16 +1,74 @@
 import { useState } from "react";
+import placeholder from '../assets/plantImages/placeholder.jpg'
 
-function AddPlantForm() {
+function AddPlantForm({plants, addPlant, close}) {
+    const [formData, setFormData] = useState({
+        name: '',
+        species: '',
+        wateringFrequency: '',
+        lightingRequirement: '',
+        isToxic: false
+    })
 
+    //this handleChange function is diff. from the contact form because it checks the type of input first since a checkbox is boolean
     const handleChange = (e) => {
-        const {name, value} = e.target;
+        const {name, value, type, checked} = e.target;
         setFormData((prevData) => ({
             ...prevData,
-            [name]:value,
+            [name]: type === 'checkbox' ? checked : value
         }))
     }
-    return (
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const newPlant = {
+            id: plants.length + 1,
+            name: formData.name,
+            species: formData.species,
+            wateringFrequency: Number(formData.wateringFrequency),
+            lightRequirement: formData.lightRequirement,
+            isToxic: formData.isToxic,
+            health: 100,
+            lastWatered: new Date().toLocaleDateString('en-US'),
+            image: placeholder,
+            imageAlt: 'Placeholder image for ${formData.name}'
+        }
+
+        addPlant(newPlant)
+        close()
+    }
+
+    return (
+        <section>
+            <h3>Add a Plant</h3>
+            <form onSubmit={handleSubmit}>
+                <label>Plant Name:
+                    <input type="text" name="name" value={formData.name} onChange={handleChange} placeholder="e.g. Orchid" required />
+                </label><br />
+                <label>Plant Species:
+                    <input type="text" name="species" value={formData.species} onChange={handleChange} placeholder="e.g. Phalaenopsis" required />
+                </label><br />
+                <label>Watering Frequency (in days):
+                    <input type="number" name="wateringFrequency" value={formData.wateringFrequency} onChange={handleChange} min={1} max={30} required />
+                </label><br />
+                <label>Light Requirement:
+                    <select name="lightRequirement" value={formData.lightRequirement} onChange={handleChange} required>
+                        <option value=''>-- Select a light requirement --</option>
+                        <option value='Low'>Low</option>
+                        <option value='Low to Medium'>Low to Medium</option>
+                        <option value='Medium Indirect'>Medium Indirect</option>
+                        <option value='Medium to Bright Indirect'>Medium to Bright Indirect</option>
+                        <option value='Bright Indirect'>Bright Indirect</option>
+                        <option value='Bright Direct'>Bright Direct</option>
+                    </select>
+                </label><br />
+                <label>Toxic to Pets?
+                    <input type="checkbox" name="isToxic" checked={formData.isToxic} onChange={handleChange} />
+                </label><br />
+                <button type="submit">Add Plant</button>
+            </form>
+        </section>
     )
 }
 
