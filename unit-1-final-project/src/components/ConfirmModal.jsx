a86436f4577de537959d8369d3fc8c8c1a890158
fedallxn <faithe.dall@gmail.com>
2026-04-01 20:27:13 -0500

    Totally forgot to add the return statment to the modal which is why it wasn't rendering when the remove button was clicked. Also added some styling so that the message actually overlays using the z-index! Fixed spacing between plant cards as well so that they sit in rows instead on on top of each other in a straight line

--- unit-1-final-project/src/components/ConfirmModal.jsx
+++ unit-1-final-project/src/components/ConfirmModal.jsx
@@ -1,14 +1,16 @@
 //this is my pop-up message that will appear after a user wants to delete a plant, to "confirm" they actually want to remove it
 function ConfirmModal({message, confirm, cancel}) {
-    <div className="modal-overlay">
-        <div className="modal-message">
-            <p>{message}</p>
-            <div className="modal-buttons">
-                <button onClick={confirm}>Yes, please!</button>
-                <button onClick={cancel}>Actually...</button>
+    return (
+        <div className="modal-overlay">
+            <div className="modal-message">
+                <p>{message}</p>
+                <div className="modal-buttons">
+                    <button onClick={confirm}>Yes, please!</button>
+                    <button onClick={cancel}>Actually...</button>
+                </div>
             </div>
         </div>
-    </div>
+    )
 }
 
 export default ConfirmModal;
--- unit-1-final-project/src/components/PlantCollection.jsx
+++ unit-1-final-project/src/components/PlantCollection.jsx
@@ -2,7 +2,7 @@ import PlantCard from "./PlantCard";
 
 function PlantCollection({plants, deletePlant}) {
     return (
-        <section>
+        <section className="plant-collection">
             {plants.length === 0 ? <p>No plants yet! Add your first plant!</p> : 
                 plants.map((plant) => (
                     <PlantCard key={plant.id} plant={plant} deletePlant={deletePlant} />
